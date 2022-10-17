c7163492c307fb9bbd9790523060799fa107a7bb
marko.novakovic <marko.novakovic@levi9.com>
2022-10-17 13:33:37 +0200

    finished

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,71 +1,22 @@
-import React, { useState, useEffect, useContext } from 'react'
+import React, { useContext } from 'react'
 import { AppLayout } from 'components/Layouts'
 import Button from 'components/Button'
-import { CartContext } from 'context/cart/CartContext';
-import fakedata from 'fakedata';
+import { CounterContext } from 'context/counter/CounterContext';
+import Headline from 'components/Headline';
 
 const Home = () => {
-    const [ item, setItem] = useState(false)
-    const { cartState, addToCart, removeCartItem, updateQuantity } = useContext( CartContext );
-
-    console.log(cartState, 'todo')
-  useEffect(() => {
-    getAllProducts();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[])
    
-  const getAllProducts = async () => {
-    try {
-        //const res = await productsService.fetchAllProducts();
-        //const res = await  authService.signin('mor_2314', '83r5^_');
-        // const data = jwtDecoder(res.data.token);
-   
-    } catch (error) {
-      
-    }
-  }
+  const { counterState, addition, subtraction } = useContext( CounterContext );
+  const { total } = counterState;
   return (
     <AppLayout>
-        Home
-        <Button onClick={() =>{
-          addToCart({
-            "id": 1,
-            "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-            "price": 109.95,
-             "quantity":1,
-            "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-          
-          },)
-            setItem(!item);
-        }}> Click</Button><br />
-       <ul>
-          {
-            fakedata.map((item) => {
-              return(
-                <li key={item.id}>
-                  {item.title} price: {item.price}{""}
-                  <Button onClick={() =>   addToCart({...item, quantity:0})}>Add</Button>{' '}
-                </li>
-              )
-            })
-          }
-       </ul>
-       <hr />
-       <ul>
-          {
-            cartState.items.map((item) => {
-              return(
-                <li key={item.id}>
-                  {item.title} quantity: {item.quantity} {""}
-                  <Button onClick={() => updateQuantity(item.id, 'plus')}>Plus</Button>{' '}
-                  <Button onClick={() => updateQuantity(item.id, 'minus')}>MInus</Button> {" "}
-                  <Button onClick={() => removeCartItem(item.id)}>Remove</Button>
-                </li>
-              )
-            })
-          }
-       </ul>
-       <h2>{cartState.amount}</h2>
+      <Headline title='Home' />
+      <div>
+        Context example:
+        <p>Total: {total}</p>
+        <Button onClick={subtraction}>-</Button> {" "}
+        <Button onClick={addition}>+</Button>
+       </div>
     </AppLayout>
   )
 }
